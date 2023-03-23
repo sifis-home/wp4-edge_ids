07f2bcdcdf8aa0ce7b3ab95a1c8f110b07783a4e
Ossi Saukko <ossi.saukko@centria.fi>
2023-03-23 09:46:38 +0200

    Testing the Testing Endpoint
    The testing endpoint allows a developer to send test alarm messages. The
    added test sends default and custom alarm messages and checks that they
    are inserted into the database.

--- src/api_v1/testing.rs
+++ src/api_v1/testing.rs
@@ -84,3 +84,75 @@ pub async fn send_test_alarm(
         false => Err(Status::InternalServerError),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::api_v1::tests_common::TestSetup;
+    use crate::structures::statistics::{AlarmMessages, AlertStatus, MessageType, Stat};
+    use rocket::http::Status;
+
+    // This test does the following:
+    //
+    // 1. Inserts default alarm message
+    // 2. Inserts custom alarm message
+    // 3. Waits for one second to database writes to complete
+    // 4. Checks that alarms are available
+    #[tokio::test]
+    async fn test_testing() {
+        let setup = TestSetup::new().await;
+        let client = &setup.client;
+        let uri = "/v1/netspots/test/alarm";
+
+        // 1. Inserts default alarm message
+        let response = client.post(uri).dispatch().await;
+        assert_eq!(response.status(), Status::Created);
+
+        // 2. Inserts custom alarm message
+        let response = client
+            .post("/v1/netspots/test/alarm")
+            .body(
+                r#"{
+	"name": "Test",
+	"stat": "PERF",
+	"status": "UP_ALERT",
+	"value": 12.3,
+	"probability": 0.75
+}"#,
+            )
+            .dispatch()
+            .await;
+        assert_eq!(response.status(), Status::Created);
+
+        // 3. Waits for one second to database writes to complete
+        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+
+        // 4. Checks that alarms are available
+        let response = client.get("/v1/netspots/alarms").dispatch().await;
+        assert_eq!(response.status(), Status::Ok);
+        let messages = response
+            .into_json::<AlarmMessages>()
+            .await
+            .expect("Valid JSON");
+        assert_eq!(messages.len(), 2);
+
+        assert_eq!(messages[0].name, "Test alarm");
+        assert_eq!(messages[0].stat, Stat::RSyn);
+        assert_eq!(messages[0].status, AlertStatus::UpAlert);
+        assert_eq!(messages[0].value, 1000.0);
+        assert_eq!(messages[0].probability, 0.75);
+        assert_eq!(messages[0].series, "TEST ALARM");
+        assert_eq!(messages[0].code, 1);
+        assert_eq!(messages[0].msg_type, MessageType::Alarm);
+
+        assert_eq!(messages[1].name, "Test");
+        assert_eq!(messages[1].stat, Stat::Perf);
+        assert_eq!(messages[1].status, AlertStatus::UpAlert);
+        assert_eq!(messages[1].value, 12.3);
+        assert_eq!(messages[1].probability, 0.75);
+        assert_eq!(messages[1].series, "TEST ALARM");
+        assert_eq!(messages[1].code, 1);
+        assert_eq!(messages[1].msg_type, MessageType::Alarm);
+
+        setup.cleanup().await;
+    }
+}
